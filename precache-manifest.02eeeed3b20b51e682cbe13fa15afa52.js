72a340a17915fd43a024c8e909c1f8e4f5128b73
Muskan Khandelwal <kmuskan@uber.com>
2020-05-30 16:56:30 +0530

    Updates

---
+++ precache-manifest.02eeeed3b20b51e682cbe13fa15afa52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7420894b68d4011c8cd3b625b0c8e30d",
+    "url": "/kmuskan/MapTrial/index.html"
+  },
+  {
+    "revision": "f3d796f16d773678db39",
+    "url": "/kmuskan/MapTrial/static/css/main.c522ac9f.chunk.css"
+  },
+  {
+    "revision": "fcb5d167d0db885108ff",
+    "url": "/kmuskan/MapTrial/static/js/2.ec9d4a2b.chunk.js"
+  },
+  {
+    "revision": "f3d796f16d773678db39",
+    "url": "/kmuskan/MapTrial/static/js/main.28d07579.chunk.js"
+  },
+  {
+    "revision": "b235e71db529d7a5ab70",
+    "url": "/kmuskan/MapTrial/static/js/runtime~main.83cf6c29.js"
+  }
+]);
